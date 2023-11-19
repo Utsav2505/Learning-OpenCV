59841c80cf03de1df571b49ae24345d45b469cea
utsav <utsav2005maji@gmail.com>
2023-11-19 11:56:10 +0530

    added function to rescale frame

--- read_video.py
+++ read_video.py
@@ -3,6 +3,7 @@
 import cv2 as cv
 
 #creating a function to rescale the frames of the video
+#will work for images, video, livecapture
 def rescaleFrame(frame,scale = 0.6):
     width = int (frame.shape[1]*scale) # shape is a A tuple representing the dimensions of the frame in the format (height, width, channels)
     height = int (frame.shape[0]*scale)
@@ -10,8 +11,15 @@ def rescaleFrame(frame,scale = 0.6):
     dimensions = (width,height) #creating a new tuple
     return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA) #interpolation=cv.INTER_AREA argument specifies the interpolation method to be used during resizing (in this case, INTER_AREA).
 
+#creating a function to change resolution
+def changeRes(width,height):
+    #will only work for live capture
+    capture.set(3,width)
+    capture.set(4,height)
+    #we can use this function in place of rescale frame for changing resolution of live capture 
+
 #Input video
-capture = cv.VideoCapture('Videos/video.mp4')
+capture = cv.VideoCapture('Videos/video.mp4')#use an integer(0,1,2,3) to select a webcam/camera in place of the path for live capture.
 
 #using while loop to read video frame by frame and displaying
 while True:
