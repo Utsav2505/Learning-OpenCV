965ca932f3a094576a516372d140284127c408c3
utsav <utsav2005maji@gmail.com>
2023-11-19 11:26:48 +0530

    rescaled frame

--- read.py
+++ read.py
@@ -1,10 +1,18 @@
 import cv2 as cv
 
+def rescaleFrame(frame,scale = 0.6):
+    width = int (frame.shape[1]*scale)
+    height = int (frame.shape[0]*scale)
+
+    dimensions = (width,height)
+    return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
+
 capture = cv.VideoCapture('Videos/video.mp4')
 
 while True:
     isTrue, frame = capture.read()
-    cv.imshow('video',frame)
+    frame_resize = rescaleFrame(frame)
+    cv.imshow('video',frame_resize)
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
 capture.release()
