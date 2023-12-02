0897ba7bd148b3bfdef1bddaeefd4adc270e0195
utsav <utsav2005maji@gmail.com>
2023-12-02 16:25:44 +0530

    starting contours detection

---
+++ contours.py
@@ -0,0 +1,15 @@
+#Learning Contour detection using Open CV
+#date- 2 Dec, 2023
+import cv2 as cv
+
+#reading an image
+img = cv.imread('Photos/example.JPG')
+
+#output/display image
+cv.imshow('dew',img)
+
+#converting this into grayscale
+
+
+#waiting for 0ms
+cv.waitKey(0)
