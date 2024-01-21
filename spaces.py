54237cf6f32674e77759c5f2cac6eb93f5819c23
vikas5431 <vs77777723@gmail.com>
2024-01-21 16:01:29 +0530

    lab color done

--- spaces.py
+++ spaces.py
@@ -1,9 +1,11 @@
 
 #date-21 Jan,2023
 import cv2 as cv
+import matplotlib.pylot as plt
 
 img =cv.imread('Photos/example.jpg');
 cv.imshow('example',img);
+#openCV reads in BGR(Blue,Green,Red) format
 
 #BGR to Grayscale
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
@@ -13,4 +15,11 @@ cv.imshow("gray",gray)
 hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
 cv.imshow("hsv",hsv)
 
+#BGR to L*A*B
+lab = cv.cvtColor(img,cv.COLOR_BGR2LAB)
+cv.imshow("lab",lab)
+
+#using matplotlib to read-display image
+plt.imshow(img)
+plt.show()
 cv.waitKey(0)
