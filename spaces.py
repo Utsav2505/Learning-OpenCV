24e0d60392ea68f5efbeda7f9b0fa2ef8e543b33
vikas5431 <vs77777723@gmail.com>
2024-01-21 18:20:33 +0530

    commit

--- spaces.py
+++ spaces.py
@@ -1,25 +1,30 @@
 
 #date-21 Jan,2023
 import cv2 as cv
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
 
 img =cv.imread('Photos/example.jpg');
-cv.imshow('example',img);
+img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+cv.imshow('example',img)
+cv.waitKey(0)
+plt.imshow(img_rgb)
+plt.show()
+# cv.imshow('example',img)
 #openCV reads in BGR(Blue,Green,Red) format
 
-#BGR to Grayscale
-gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-cv.imshow("gray",gray)
+# #BGR to Grayscale
+# gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# cv.imshow("gray",gray)
 
-#BGR to HSV(hue saturation value)
-hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
-cv.imshow("hsv",hsv)
+# #BGR to HSV(hue saturation value)
+# hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+# cv.imshow("hsv",hsv)
 
-#BGR to L*A*B
-lab = cv.cvtColor(img,cv.COLOR_BGR2LAB)
-cv.imshow("lab",lab)
+# #BGR to L*A*B
+# lab = cv.cvtColor(img,cv.COLOR_BGR2LAB)
+# cv.imshow("lab",lab)
 
 #using matplotlib to read-display image
-plt.imshow(img)
-plt.show()
-cv.waitKey(0)
+
+# cv.waitKey(0)
