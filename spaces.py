d8e2997915ccf15a8563e71ffaa6bc5cce3544fd
Utsav <utsav2005maji@gmail.com>
2024-01-23 22:44:53 +0530

    done spaces

--- spaces.py
+++ spaces.py
@@ -6,13 +6,14 @@ import matplotlib.pyplot as plt
 img =cv.imread('E:\Documents\Python\Open CV\Learning\Photos\example.JPG')
 #matplotlib will show inverted color as opencv is  loading in BGR
 
-#BGR to RGB
-img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+# #BGR to RGB
+# img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-cv.imshow('example',img)
-cv.waitKey(0)
-plt.imshow(img_rgb)
-plt.show()
+#using matplotlib to read-display image
+# cv.imshow('example',img)
+# cv.waitKey(0)
+# plt.imshow(img_rgb)
+# plt.show()
 
 # cv.imshow('example',img)
 #openCV reads in BGR(Blue,Green,Red) format
@@ -22,13 +23,15 @@ plt.show()
 # cv.imshow("gray",gray)
 
 # #BGR to HSV(hue saturation value)
-# hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
-# cv.imshow("hsv",hsv)
+hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+cv.imshow("hsv",hsv)
 
 # #BGR to L*A*B
 # lab = cv.cvtColor(img,cv.COLOR_BGR2LAB)
 # cv.imshow("lab",lab)
 
-#using matplotlib to read-display image
 
-# cv.waitKey(0)
+#HSV to BGR
+bgr = cv.cvtColor(hsv,cv.COLOR_HSV2BGR)
+cv.imshow('bgr',bgr)
+cv.waitKey(0)
