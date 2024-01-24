74739e236d4a023f6141cac36df954aa41aa8f6f
Utsav <utsav2005maji@gmail.com>
2024-01-24 11:34:33 +0530

    starting histogram

---
+++ histogram.py
@@ -0,0 +1,9 @@
+#program to learn histogram
+#date-24 Jan,2023
+import cv2 as cv
+import numpy as np
+
+# read a image and display normally
+img =cv.imread('E:\Documents\Python\Open CV\Learning\Photos\example.JPG')
+cv.imshow('img',img)
+
