68a7a2aaaa2cfc81f64518a26c181d03a00689f4
Utsav <utsav2005maji@gmail.com>
2024-01-30 19:22:33 +0530

    thresh done

--- thresh.py
+++ thresh.py
@@ -13,8 +13,19 @@ cv.imshow('img',img)
 
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow('gray',gray)
+
 # simple thresholding
 threshold,thresh = cv.threshold(gray,150,255,cv.THRESH_BINARY)
-cv.imshow('thresh',thresh)
+cv.imshow('simple threshold',thresh)
 
-cv.waitkey(0)
+threshold,thresh_inv = cv.threshold(gray,150,255,cv.THRESH_BINARY_INV)
+cv.imshow('simple threshold inverse',thresh_inv)
+
+# Adaptive threshold
+adaptive_thresh = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,3)
+cv.imshow('adaptive threshold',adaptive_thresh)
+
+adaptive_thresh_inv = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY_INV,11,3)
+cv.imshow('adaptive threshold inverse',adaptive_thresh_inv)
+
+cv.waitKey(0)
